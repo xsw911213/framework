99e86e67c45b550a75416d5a73cd55adc2aff4c5
xushaowei <xushaowei10077@autohome.com.cn>
2018-03-08 16:07:09 +0800

    修改样式问题

--- gulp-dev.js
+++ gulp-dev.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var fileinclude = require('gulp-file-include');
 var browserSync = require('browser-sync').create();
+var htmlmin     = require('gulp-htmlmin');
 //var sass        = require('gulp-ruby-sass');
 var sass        = require('gulp-sass');
 var reload      = browserSync.reload;
@@ -16,8 +17,9 @@ gulp.task('serve', ['sass','fileinclude'], function() {
   });
 
   gulp.watch("./src/scss/*.scss", ['sass']);
-  gulp.watch("./src/html/*.html",['fileinclude']);
-  gulp.watch("./src/html/components/*/*.html",['fileinclude']);
+  gulp.watch("./src/_html/**.html",['fileinclude']);
+  gulp.watch("./src/_html/components/**.html",['fileinclude']);
+  gulp.watch("./src/_html/components/*/**.html",['fileinclude']);
 
   gulp.watch("./src/*.html").on('change', reload);
   gulp.watch("./src/css/*.css").on('change', reload);
@@ -28,17 +30,30 @@ gulp.task('serve', ['sass','fileinclude'], function() {
 gulp.task('sass', function() {
   return gulp.src("./src/scss/*.scss")
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest("./src/css"))
+        .pipe(gulp.dest("./src/style"))
         .pipe(reload({stream: true}));
 });
 
 gulp.task('fileinclude', function() {
-  gulp.src(['./src/html/*.html'])
+
+  var options = {
+    removeComments: true,//清除HTML注释
+    // collapseWhitespace: true,//压缩HTML
+    // collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+    // removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+    // removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+    // removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+    // minifyJS: true,//压缩页面JS
+    // minifyCSS: true//压缩页面CSS
+  };
+
+  gulp.src(['./src/_html/*.html'])
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file',
       indent: true//保留文件的缩进
     }))
+    .pipe(htmlmin(options))
     .pipe(gulp.dest('./src'))
     .pipe(reload({stream: true}));
 });
